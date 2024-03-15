0c86e514ab5182ca7eee01e7ca0da54aae82cfa2
shobitkhatri05 <shobitkhatri62@gmail.com>
2024-03-15 21:32:17 +0530

    User Register

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,16 +1,25 @@
-import {v2 as cloudinary} from "cloudinary"
+// import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs"
-
+          
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//   api_key: process.env.CLOUDINARY_API_KEY, 
+//   api_secret: process.env.CLOUDINARY_API_SECRET 
+// });
+import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+  cloud_name: 'dpjgidhtl', 
+  api_key: '576322667739139', 
+  api_secret: 'lc6OyGoFndkKAWK1aMuxB8N8ImM' 
 });
+
 
 const uploadOnCloudinary = async(localFilePath) =>{
     try {
+       
         if (!localFilePath) {
+            
             return null
         }
         //upload the file on cloudinary
@@ -18,8 +27,10 @@ const uploadOnCloudinary = async(localFilePath) =>{
             resource_type:"auto"
         })
         //file has been uploaded successfully
-        console.log("file is uploaded on cloudinary",response.url);
-        return response
+        // console.log("file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
+        // console.log(response)
+        return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
